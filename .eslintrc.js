a221173548092544a87046722895b513b8a62a04
shinobu-takahashi <shinobu_takahashi@c.dwango.co.jp>
2022-06-08 13:37:57 +0900

    use akashic/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,12 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
+    "root": true,
     "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+        "@akashic/eslint-config"
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "@typescript-eslint/consistent-type-imports": "error"
+        "project": "tsconfig.json",
+        "sourceType": "module",
+        "tsconfigRootDir": __dirname
     }
 }
